778a897fc84833231d503ccd3ea148564d2498da
NickClone <nico.c85@live.com.ar>
2015-05-03 14:26:27 -0300

    Login page
    se agrego login de usuario

---
+++ wssprueba/WcfService1/Vistas/js/control.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    
+//    $("#but").click(function () {
+//        funcion();
+//    });
+});
+
+
+
+//
+//
+//function funcion() {
+//    var per = new Object();
+//    per.id = 32;
+//    per.name = 'Nicolito';
+//    per.app = 'calvo';
+//    var det = 'alala';
+//    $.ajax({
+//        url: 'http://localhost:1066/Expensas.svc/GetExp',               //  asda 'http://localhost:1066/Service1.svc/GetData',
+//        headers:{
+//        'Accept': 'application/json',
+//        'Content-Type': 'application/json'
+//        },
+//        type: 'POST',
+//        dataType: 'JSON',
+//        data: JSON.stringify(per),
+//        async: false
+//    }).done(function (data) {
+//        alert(data.name);
+//    });
+//    //alert(ret);
+//}
